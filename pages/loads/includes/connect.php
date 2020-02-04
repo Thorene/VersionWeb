4db59339ec13de349c1032fb9fb510f335989f32
Thorene <madiansw@gmail.com>
2020-02-04 21:07:42 +0100

    Delete connect.php

--- pages/loads/includes/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-	//Ne pas oublier de retirer "array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING" quand on édite pas le site
-	//  $bdd = new PDO('mysql:host=mysql5-16.pro;dbname=monsitewxblog', 'monsitewxblog', 'LDWuEfL7', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
-	$bdd = new PDO('mysql:host=solutions-web.be;dbname=studiofr_wp49697', 'studiofr_wp49697', '6C0Sy]2g[p', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
- ?>
