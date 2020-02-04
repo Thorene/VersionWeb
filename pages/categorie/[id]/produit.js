202856164037be88dc3f91289a266cd50a4b6419
Thorene <madiansw@gmail.com>
2020-02-04 20:49:43 +0100

    dfg
    z

--- pages/categorie/[id]/produit.js
+++ pages/categorie/[id]/produit.js
@@ -1,5 +1,4 @@
-import { useRouter } from 'next/router'
-import Link from 'next/link'
+import { useRouter } from 'next/router' 
  import React from 'react'
 import Head from 'next/head'
  import Link from 'next/link';
